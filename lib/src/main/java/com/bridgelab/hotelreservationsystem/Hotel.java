3080052fc3543c144c6023082e5d2023ca7b8758
shreyasalanke <shreyasalanke97@gmail.com>
2021-07-22 23:11:25 +0530

    ADDED:Program to add Special  Rates for Customer

--- lib/src/main/java/com/bridgelab/hotelreservationsystem/Hotel.java
+++ lib/src/main/java/com/bridgelab/hotelreservationsystem/Hotel.java
@@ -1,32 +1,54 @@
 package com.bridgelab.hotelreservationsystem;
 import java.util.Objects;
 
-public class Hotel {
+public class Hotel 
+{
 
 	private String name;
 	private int regularWeekdayRate;
 	private int regularWeekendRate;
+	private int rating;
 	private int rewardsWeekdayRate;
 	private int rewardsWeekendRate;
-	private int rating;
 
-	public Hotel(String name, int regularWeekdayRate, int regularWeekendRate, int rewardsWeekdayRate,
-			int rewardsWeekendRate, int rating)
+	public Hotel(String name, int regularWeekdayRate)
+	{
+		this.name = name;
+		this.regularWeekdayRate = regularWeekdayRate;
+	}
+
+	public Hotel(String name, int regularWeekdayRate, int regularWeekendRate) 
 	{
 		this.name = name;
 		this.regularWeekdayRate = regularWeekdayRate;
 		this.regularWeekendRate = regularWeekendRate;
-		this.rewardsWeekdayRate = rewardsWeekdayRate;
-		this.rewardsWeekendRate = rewardsWeekendRate;
+	}
+
+	public Hotel(String name, int regularWeekdayRate, int regularWeekendRate, int rating) 
+	{
+		this.name = name;
+		this.regularWeekdayRate = regularWeekdayRate;
+		this.regularWeekendRate = regularWeekendRate;
 		this.rating = rating;
 	}
 
-	public String getName()
+	public Hotel(String name, int regularWeekdayRate, int regularWeekendRate, int rating, int rewardsWeekdayRate,
+			int rewardsWeekendRate) 
+	{
+		this.name = name;
+		this.regularWeekdayRate = regularWeekdayRate;
+		this.regularWeekendRate = regularWeekendRate;
+		this.rating = rating;
+		this.rewardsWeekdayRate = rewardsWeekdayRate;
+		this.rewardsWeekendRate = rewardsWeekendRate;
+	}
+
+	public String getName() 
 	{
 		return name;
 	}
 
-	public void setName(String name) 
+	public void setName(String name)
 	{
 		this.name = name;
 	}
@@ -46,22 +68,22 @@ public class Hotel {
 		return regularWeekendRate;
 	}
 
-	public void setRegularWeekendRate(int regularWeekendRate) 
+	public void setRegularWeekendRate(int regularWeekendRate)
 	{
 		this.regularWeekendRate = regularWeekendRate;
 	}
 
-	public int getRewardsWeekdayRate()
+	public int getRewardsWeekdayRate() 
 	{
 		return rewardsWeekdayRate;
 	}
 
-	public void setRewardsWeekdayRate(int rewardsWeekdayRate)
+	public void setRewardsWeekdayRate(int rewardsWeekdayRate) 
 	{
 		this.rewardsWeekdayRate = rewardsWeekdayRate;
 	}
 
-	public int getRewardsWeekendRate() 
+	public int getRewardsWeekendRate()
 	{
 		return rewardsWeekendRate;
 	}
@@ -76,7 +98,7 @@ public class Hotel {
 		return rating;
 	}
 
-	public void setRating(int rating)
+	public void setRating(int rating) 
 	{
 		this.rating = rating;
 	}
--- lib/src/test/java/com/bridgelab/hotelreservationsystem/HotelReservationTest.java
+++ lib/src/test/java/com/bridgelab/hotelreservationsystem/HotelReservationTest.java
@@ -10,15 +10,14 @@ import java.util.Map;
 
 import org.junit.Test;
 
-public class HotelReservationTest
+public class HotelReservationTest 
 {
-
 	@Test
-	public void whenHotelAddedToSystemShouldGetAdded() 
+	public void whenHotelAddedToSystemShouldGetAdded()
 	{
-		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
-		Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 110, 50, 4);
-		Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
+		Hotel hotel1 = new Hotel("Lakewood", 110);
+		Hotel hotel2 = new Hotel("Bridgewood", 160);
+		Hotel hotel3 = new Hotel("Ridgewood", 220);
 		Hotel[] hotelList = { hotel1, hotel2, hotel3 };
 		List<Hotel> hotels = Arrays.asList(hotelList);
 		HotelReservation hotelReservation = new HotelReservation();
@@ -32,9 +31,9 @@ public class HotelReservationTest
 	@Test
 	public void whenGivenDateRangeShouldReturnCheapestHotel()
 	{
-		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
-		Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 110, 50, 4);
-		Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
+		Hotel hotel1 = new Hotel("Lakewood", 110);
+		Hotel hotel2 = new Hotel("Bridgewood", 160);
+		Hotel hotel3 = new Hotel("Ridgewood", 220);
 		HotelReservation hotelReservation = new HotelReservation();
 		hotelReservation.add(hotel1);
 		hotelReservation.add(hotel2);
@@ -45,27 +44,28 @@ public class HotelReservationTest
 	}
 
 	@Test
-	public void whenGivenHotelAddedShouldAddWeekendPrices() 
+	public void whenGivenHotelAddedShouldAddWeekendPrices()
 	{
-		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
-		Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 110, 50, 4);
-		Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
+		Hotel hotel1 = new Hotel("Lakewood", 110, 90);
+		Hotel hotel2 = new Hotel("Bridgewood", 160, 60);
+		Hotel hotel3 = new Hotel("Ridgewood", 220, 150);
 		HotelReservation hotelReservation = new HotelReservation();
 		hotelReservation.add(hotel1);
 		hotelReservation.add(hotel2);
 		hotelReservation.add(hotel3);
 		List<Hotel> hotelList = hotelReservation.getHotelList();
-		boolean result = hotelList.get(0).getRegularWeekendRate() == 90
-				&& hotelList.get(1).getRegularWeekendRate() == 60 && hotelList.get(2).getRegularWeekendRate() == 150;
+		boolean result = hotelList.get(0).getRegularWeekendRate() == 90 && 
+						 hotelList.get(1).getRegularWeekendRate() == 60 && 
+						 hotelList.get(2).getRegularWeekendRate() == 150;
 		assertTrue(result);
 	}
-	
+
 	@Test
 	public void whenGivenDateRangeShouldReturnCheapestHotels() 
 	{
-		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
-		Hotel hotel2 = new Hotel("Bridgewood", 150, 50, 110, 50, 4);
-		Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
+		Hotel hotel1 = new Hotel("Lakewood", 110, 90);
+		Hotel hotel2 = new Hotel("Bridgewood", 150, 50);
+		Hotel hotel3 = new Hotel("Ridgewood", 220, 150);
 		HotelReservation hotelReservation = new HotelReservation();
 		hotelReservation.add(hotel1);
 		hotelReservation.add(hotel2);
@@ -74,29 +74,30 @@ public class HotelReservationTest
 		result.forEach((k, v) -> System.out.println(k.getName() + " " + v));
 		assertNotNull(result);
 	}
-	
+
 	@Test
 	public void whenHotelAddedToSystemRatingShouldGetAdded() 
 	{
-		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
-		Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 110, 50, 4);
-		Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
+		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 3);
+		Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 4);
+		Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 5);
 		HotelReservation hotelReservation = new HotelReservation();
 		hotelReservation.add(hotel1);
 		hotelReservation.add(hotel2);
 		hotelReservation.add(hotel3);
 		List<Hotel> hotelList = hotelReservation.getHotelList();
-		boolean result = hotelList.get(0).getRating() == 3 && hotelList.get(1).getRating() == 4
-				&& hotelList.get(2).getRating() == 5;
+		boolean result = hotelList.get(0).getRating() == 3 && 
+						 hotelList.get(1).getRating() == 4 && 
+						 hotelList.get(2).getRating() == 5;
 		assertTrue(result);
 	}
-	
+
 	@Test
 	public void whenGivenDateRangeShouldReturnCheapestBestRatedHotels()
 	{
-		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
-		Hotel hotel2 = new Hotel("Bridgewood", 150, 50, 110, 50, 4);
-		Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
+		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 3);
+		Hotel hotel2 = new Hotel("Bridgewood", 150, 50, 4);
+		Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 5);
 		HotelReservation hotelReservation = new HotelReservation();
 		hotelReservation.add(hotel1);
 		hotelReservation.add(hotel2);
@@ -106,13 +107,13 @@ public class HotelReservationTest
 				(k, v) -> System.out.println(k.getName() + ", Rating : " + k.getRating() + " and Total Rate " + v));
 		assertNotNull(result);
 	}
-	
+
 	@Test
 	public void whenGivenDateRangeShouldReturnBestRatedHotels() 
 	{
-		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
-		Hotel hotel2 = new Hotel("Bridgewood", 150, 50, 110, 50, 4);
-		Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
+		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 3);
+		Hotel hotel2 = new Hotel("Bridgewood", 150, 50, 4);
+		Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 5);
 		HotelReservation hotelReservation = new HotelReservation();
 		hotelReservation.add(hotel1);
 		hotelReservation.add(hotel2);
@@ -121,5 +122,25 @@ public class HotelReservationTest
 		result.forEach(
 				(k, v) -> System.out.println(k.getName() + ", Rating : " + k.getRating() + " and Total Rate " + v));
 		assertNotNull(result);
+	}
+	
+	@Test
+	public void whenHotelsAddedShouldAddRewardsCustomerRate() 
+	{
+		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 3, 80, 80);
+		Hotel hotel2 = new Hotel("Bridgewood", 150, 50, 4, 110, 50);
+		Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 5, 100, 40);
+		HotelReservation hotelReservation = new HotelReservation();
+		hotelReservation.add(hotel1);
+		hotelReservation.add(hotel2);
+		hotelReservation.add(hotel3);
+		List<Hotel> hotelList = hotelReservation.getHotelList();
+		boolean result = hotelList.get(0).getRewardsWeekendRate() == 80 && 
+						 hotelList.get(0).getRewardsWeekdayRate() == 80 && 
+						 hotelList.get(1).getRewardsWeekendRate() == 50 && 
+						 hotelList.get(1).getRewardsWeekdayRate() == 110 && 
+						 hotelList.get(2).getRewardsWeekendRate() == 40 &&
+						 hotelList.get(2).getRewardsWeekdayRate() == 100;
+		assertTrue(result);
 	}
 }
